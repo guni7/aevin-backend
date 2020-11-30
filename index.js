9c5dd06ad3201ef94a39ad89ac723d5c30bdc5f2
Ubuntu <ubuntu@ip-172-31-9-210.us-east-2.compute.internal>
2020-11-30 17:35:33 +0000

    prod changes

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
-
+const https = require('https');
+const fs = require('fs')
 
 require('dotenv').config();
 require('./db/connectDB');
 
+var options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/aevin.app/privkey.pem', 'utf8'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/aevin.app/cert.pem', 'utf8'),
+    ca: fs.readFileSync('/etc/letsencrypt/live/aevin.app/chain.pem', 'utf8'),
+};
 
 //Import routes
 const authRoute = require('./routes/auth');
@@ -20,13 +26,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Route Middlewares
-app.use(cors());
+app.use(cors({ origin:true, credentials:true }));
 app.use('/api/user', authRoute);
 app.use('/', publicRoute);
 app.use('/', editRoute);
 
 
-const PORT = 5000;
+const server = https.createServer(options, app)
 
-app.listen(PORT, () => console.log('server is up and running '));
+const PORT = 8443;
 
+server.listen(PORT, () => console.log('server is up and running ' ));
